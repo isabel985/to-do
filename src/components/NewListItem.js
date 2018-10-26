6eb361a0e45f485dd6e1e779f81db78b3aacbc55
isabel985 <lee.isabel59@gmail.com>
2018-10-25 19:23:55 -0500

    Adding list items

---
+++ src/components/NewListItem.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class NewListItem extends Component {
+    // state = {
+    //     message: ''
+    // }
+
+    handleInputChange
+}
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,11 +1,11 @@
 import { DELETE_TODO } from '../actions/types';
 import initialState from '../data/listItems';
 
-export default function toDoListReducer( state = initialState, action ) {
+export default function toDoListReducer(state = initialState, action) {
     switch (action.type) {
         case DELETE_TODO:
-        return state.filter(listItem => listItem.id !== action.payload.id);
+            return state.filter(listItem => listItem.id !== action.payload.id);
         default:
-        return state;
+            return state;
     }
 }
